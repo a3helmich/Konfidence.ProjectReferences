5b7520442b945d01fbfa000ff83e4314f5aeb7c3
A3 <adrie@konfidence.nl>
2021-03-30 13:33:55 +0200

    unittest cleanup

--- rd-deep.UnitTest/CopyFilesTests.cs
+++ rd-deep.UnitTest/CopyFilesTests.cs
@@ -1,3 +1,8 @@
+using System;
+using System.IO;
+using System.Linq;
+using JetBrains.Annotations;
+using Konfidence.Base;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace rd_deep.UnitTest
@@ -5,10 +10,22 @@ namespace rd_deep.UnitTest
     [TestClass]
     public class CopyFilesTests
     {
+        private static string _testProjectFolder = string.Empty;
+
+        [ClassInitialize]
+        public static void ClassInitialize([NotNull] TestContext testContext)
+        {
+            var deployment = testContext.DeploymentDirectory.Split(Path.DirectorySeparatorChar, StringSplitOptions.RemoveEmptyEntries).TrimList();
+
+            var deploymentParts = deployment.TakeWhile(y => y != "bin").ToList();
+
+            _testProjectFolder = string.Join(Path.DirectorySeparatorChar, deploymentParts);
+        }
+
         [TestInitialize]
         public void TestInitialize()
         {
-            var totalFiles = CopyFilePresenter.CopyFiles(@"C:\Projects\Producten\ProjectReferences\rd-deep\Test", @"C:\Projects\Producten\ProjectReferences\rd-deep\Test", "Test");
+            var totalFiles = CopyFilePresenter.CopyFiles(Path.Combine(_testProjectFolder, "Test"), Path.Combine(_testProjectFolder, "Test"), "Test");
         }
 
         [TestMethod]
